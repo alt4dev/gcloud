438d7f224c6d7338f0d258a3053502aadcf2a5e3
Billcountry <me@billcountry.tech>
2020-08-01 17:53:53 +0300

    Test Groping

--- logging/groups_test.go
+++ logging/groups_test.go
@@ -1,6 +1,8 @@
 package logging
 
 import (
+	"bytes"
+	"os"
 	"runtime/debug"
 	"strings"
 	"testing"
@@ -17,5 +19,56 @@ func TestRoutineId(t *testing.T) {
 
 	if routineId != getRoutineId() {
 		t.Error("Continuous calls to getRoutineId() should return the same Id")
+	}
+}
+
+func TestGrouping(t *testing.T){
+	defer CloseGroup()
+	// Calling groupId without Initializing a group will create a different group on every call
+	if getGroupId() == getGroupId() {
+		t.Error("If group is not initialized, call to groupId should return a unique value every time")
+	}
+
+	InitGroup(nil, nil)
+
+	groupId, ok := logGroups[getRoutineId()]
+	if !ok {
+		t.Error("Unable to find initialized group")
+	}
+	if getGroupId() != groupId || getGroupId() != getGroupId() {
+		t.Error("After group initialization, all calls to getGroupId should return the same groupId")
+	}
+
+	// Capture logs from here
+	var buffer bytes.Buffer
+	alt4warning.SetOutput(&buffer)
+	defer func(){
+		alt4warning.SetOutput(os.Stderr)
+	}()
+
+	// Reinitializing a group should delete older group and start a new one
+	InitGroup(nil, nil)
+	// Confirm that we warned the user
+	warning := "Unclosed log group detected. Call `defer CloseGroup()` after `InitGroup()` to avoid memory leaks"
+	if !strings.Contains(buffer.String(), warning){
+		t.Error("Replacing a log group should warn the user of the previously unclosed log group")
+	}
+
+	if groupId == getGroupId() {
+		t.Error("Initializing a log group should use a new groupId")
+	}
+}
+
+func TestGroupClosing(t *testing.T){
+	InitGroup(nil, nil)
+	routine := getRoutineId()
+	if _, ok := logGroups[routine]; !ok {
+		t.Error("Unable to find initialized group")
+	}
+
+	CloseGroup()
+	// Confirm that we deleted the group from memory
+	if _, ok := logGroups[routine]; ok {
+		t.Error("Group found after closing")
 	}
 }
