3dfe5c5dac74b6d8f558065a3470c34f9795d083
Billcountry <me@billcountry.tech>
2020-08-01 16:32:06 +0300

    Test float and unique values for int

--- logging/claims_test.go
+++ logging/claims_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func sortClaims (claims []protoClaim) []protoClaim{
+func sortClaims(claims []protoClaim) []protoClaim {
 	sort.Slice(claims, func(i, j int) bool {
 		return claims[i].Name < claims[j].Name
 	})
@@ -19,11 +19,7 @@ func TestParseString(t *testing.T) {
 	})
 
 	expectedClaims := []protoClaim{
-		{
-			Name:     "name",
-			DataType: 4,
-			Value:    "John Doe",
-		},
+		{Name: "name", DataType: 4, Value: "John Doe"},
 	}
 	if !cmp.Equal(alt4Claims, expectedClaims) {
 		t.Error("Claims are not equal\n", cmp.Diff(alt4Claims, expectedClaims))
@@ -31,42 +27,59 @@ func TestParseString(t *testing.T) {
 }
 
 func TestParsIntegers(t *testing.T) {
-	var ui uint = 18
-	var ui8 uint8 = 18
-	var ui16 uint16 = 18
-	var ui32 uint32 = 18
-	var ui64 uint64 = 18
-	var i int = 18
-	var i8 int8 = 18
-	var i16 int16 = 18
-	var i32 int32 = 18
-	var i64 int64 = 18
+	var ui uint = 1000000000
+	var ui8 uint8 = 100
+	var ui16 uint16 = 1000
+	var ui32 uint32 = 10000
+	var ui64 uint64 = 100000
+	var i int = -1000000000
+	var i8 int8 = -100
+	var i16 int16 = -1000
+	var i32 int32 = -10000
+	var i64 int64 = -100000
 	alt4Claims := sortClaims(parseClaims(Claim{
-		"uint": ui,
-		"uint8": ui8,
+		"uint":   ui,
+		"uint8":  ui8,
 		"uint16": ui16,
 		"uint32": ui32,
 		"uint64": ui64,
-		"int": i,
-		"int8": i8,
-		"int16": i16,
-		"int32": i32,
-		"int64": i64,
+		"int":    i,
+		"int8":   i8,
+		"int16":  i16,
+		"int32":  i32,
+		"int64":  i64,
 	}))
 	expectedClaims := sortClaims([]protoClaim{
-		{ Name: "uint", DataType: 1, Value: "18"},
-		{ Name: "uint8", DataType: 1, Value: "18"},
-		{ Name: "uint16", DataType: 1, Value: "18"},
-		{ Name: "uint32", DataType: 1, Value: "18"},
-		{ Name: "uint64", DataType: 1, Value: "18"},
-		{ Name: "int", DataType: 1, Value: "18"},
-		{ Name: "int8", DataType: 1, Value: "18"},
-		{ Name: "int16", DataType: 1, Value: "18"},
-		{ Name: "int32", DataType: 1, Value: "18"},
-		{ Name: "int64", DataType: 1, Value: "18"},
+		{Name: "uint", DataType: 1, Value: "1000000000"},
+		{Name: "uint8", DataType: 1, Value: "100"},
+		{Name: "uint16", DataType: 1, Value: "1000"},
+		{Name: "uint32", DataType: 1, Value: "10000"},
+		{Name: "uint64", DataType: 1, Value: "100000"},
+		{Name: "int", DataType: 1, Value: "-1000000000"},
+		{Name: "int8", DataType: 1, Value: "-100"},
+		{Name: "int16", DataType: 1, Value: "-1000"},
+		{Name: "int32", DataType: 1, Value: "-10000"},
+		{Name: "int64", DataType: 1, Value: "-100000"},
 	})
 
 	if !cmp.Equal(alt4Claims, expectedClaims) {
 		t.Error("Claims are not equal Expected:\n", expectedClaims, "Got:\n", alt4Claims, "Diff:\n", cmp.Diff(alt4Claims, expectedClaims))
+	}
+}
+
+func TestParseFloat(t *testing.T) {
+	var f32 float32 = 35.6
+	var f64 float64 = 77.8
+	alt4Claims := sortClaims(parseClaims(Claim{
+		"float32": f32,
+		"float64": f64,
+	}))
+
+	expectedClaims := sortClaims([]protoClaim{
+		{Name: "float32", DataType: 2, Value: "35.6"},
+		{Name: "float64", DataType: 2, Value: "77.8"},
+	})
+	if !cmp.Equal(alt4Claims, expectedClaims) {
+		t.Error("Claims are not equal\n", cmp.Diff(alt4Claims, expectedClaims))
 	}
 }
