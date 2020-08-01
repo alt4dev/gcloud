aac3667e448fbfa5413f71f0eccc5ccdf101ef70
Billcountry <me@billcountry.tech>
2020-08-01 16:38:18 +0300

    Remove unused variables

--- logging/groups.go
+++ logging/groups.go
@@ -2,22 +2,16 @@ package logging
 
 import (
 	"github.com/google/uuid"
-	"log"
-	"os"
 	"runtime/debug"
 	"strings"
 )
 
 var (
-	alt4error *log.Logger
-	alt4warning *log.Logger
 	instanceId string
 	logGroups map[string]string
 )
 
 func init()  {
-	alt4error = log.New(os.Stderr, "[alt4](if seeing this please contact: critical@alt4.dev) ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-	alt4warning = log.New(os.Stderr, "[alt4] WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	instanceId = uuid.New().String()
 	logGroups = make(map[string]string, 0)
 }
