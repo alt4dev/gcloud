1b71fa558c6a4e5ad91fdd463f14414ac08b0a32
Billcountry <me@billcountry.tech>
2020-10-04 15:17:28 +0300

    Service implementation

--- service/init.go
+++ service/init.go
@@ -1,1 +1,102 @@
 package service
+
+import (
+	"encoding/json"
+	"github.com/alt4dev/protobuff/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"io/ioutil"
+	"os"
+)
+
+var address = "rpc.alt4.dev"
+var connection *grpc.ClientConn
+var client *proto.LoggingClient
+
+type Alt4Options struct {
+	AuthToken string
+	Mode string
+	Sink string
+	EmmitFile *os.File
+}
+
+var options = Alt4Options{
+	AuthToken: "",
+	Mode: "release",
+	Sink: "default",
+	EmmitFile: os.Stderr,
+}
+
+func init() {
+	// Initialize client on connection
+	getClient()
+	// Setup options from env
+	setupOptions()
+}
+
+// getClient connects to alt4 and creates a new logging client or reuses an existing connection
+func getClient() *proto.LoggingClient {
+	if connection == nil || client == nil {
+		var err error
+		transCert := grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(nil, "rpc.alt4.dev"))
+		connection, err = grpc.Dial(address, transCert)
+		if err != nil {
+			EmitError.Println("Error creating a connection to alt4. Error: ", err)
+			connection = nil
+			client = nil
+			return nil
+		}
+		c := proto.NewLoggingClient(connection)
+		client = &c
+	}
+	return client
+}
+
+func setupOptions() {
+	optionsFile := os.Getenv("ALT4_CONFIG")
+	if optionsFile != "" {
+		jsonContent, err := ioutil.ReadFile(optionsFile)
+		if err != nil {
+			EmitError.Print("Error opening file provided in ALT4_CONFIG. Error: ", err)
+		}else {
+			content := struct {
+				Token     string `json:"token" binding:"required"`
+				Mode      string `json:"mode"`
+				Sink		string `json:"sink"`
+			}{}
+			err = json.Unmarshal(jsonContent, &content)
+			if err != nil {
+				EmitError.Println("Error decoding ALT4_CONFIG. Error: ", err)
+			}else {
+				SetAuthToken(content.Token)
+				SetMode(content.Mode)
+				SetSink(content.Sink)
+			}
+		}
+	}
+	SetAuthToken(os.Getenv("ALT4_AUTH_TOKEN"))
+	SetMode(os.Getenv("ALT4_MODE"))
+	SetSink(os.Getenv("ALT4_SINK"))
+}
+
+func SetAuthToken(token string) {
+	if token != "" {
+		options.AuthToken = token
+	}
+}
+
+func SetMode(mode string) {
+	if mode == "release" || mode == "debug" {
+		options.Mode = mode
+	}
+}
+
+func SetSink(sink string) {
+	if sink != "" {
+		options.Sink = sink
+	}
+}
+
+func SetDebugOutput(file *os.File){
+	options.EmmitFile = file
+}
--- service/log.go
+++ service/log.go
@@ -1,1 +1,50 @@
 package service
+
+import (
+	"context"
+	"errors"
+	"github.com/alt4dev/protobuff/proto"
+	"sync"
+	"time"
+)
+
+// LogResult Object returned when you create a log entry.
+type LogResult struct {
+	wg sync.WaitGroup
+	result *proto.Result
+	error error
+}
+
+// Result Returns actual result from alt4. This will block and wait for the result if not done
+func (result LogResult) Result() (*proto.Result, error) {
+	result.wg.Wait()
+	return result.result, result.error
+}
+
+func Log(threadId string, sinkId string, threadInit bool, message string, claims []*proto.Claim, file string, line uint32, function string, level uint8) LogResult {
+	msg := proto.Message{
+		ThreadId:   threadId,
+		SinkId:     sinkId,
+		ThreadInit: threadInit,
+		Message:    message,
+		Claims:     claims,
+		FileName:   file,
+		LineNo:     line,
+		Function: 	function,
+		Level:      uint32(level),
+		Timestamp:  uint64(time.Now().UnixNano()),
+		AuthToken:  options.AuthToken,
+	}
+
+	result := LogResult{}
+	result.wg.Add(1)
+	go func() {
+		defer result.wg.Done()
+		if getClient() == nil {
+			result.error = errors.New("error connecting to remote server")
+			return
+		}
+		result.result, result.error = (*client).Log(context.Background(), &msg)
+	}()
+	return result
+}
