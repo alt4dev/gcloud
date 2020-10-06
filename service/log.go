af034042f95b2cf608a5fae3167158032f720b2f
Billcountry <me@billcountry.tech>
2020-10-06 11:44:51 +0300

    Get function, line and file from runtime

--- service/log.go
+++ service/log.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"github.com/alt4dev/protobuff/proto"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -21,15 +22,23 @@ func (result LogResult) Result() (*proto.Result, error) {
 	return result.result, result.error
 }
 
-func Log(threadId string, sinkId string, threadInit bool, message string, claims []*proto.Claim, file string, line uint32, function string, level uint8) LogResult {
+// Log Creates a log entry and writes it to alt4 in the background.
+// This function should not be called directly and should instead be used from helper functions under the `log` package.
+func Log(threadInit bool, message string, claims []*proto.Claim, level uint8) LogResult {
+	if threadInit {
+		initGroup()
+	}
+	// Get the parent file and function of the caller
+	pc, file, line, _ := runtime.Caller(2)
+	function := runtime.FuncForPC(pc).Name()
 	msg := proto.Message{
-		ThreadId:   threadId,
-		SinkId:     sinkId,
+		ThreadId:   getThreadId(),
+		SinkId:     options.Sink,
 		ThreadInit: threadInit,
 		Message:    message,
 		Claims:     claims,
 		FileName:   file,
-		LineNo:     line,
+		LineNo:     uint32(line),
 		Function: 	function,
 		Level:      uint32(level),
 		Timestamp:  uint64(time.Now().UnixNano()),
