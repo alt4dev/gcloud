4cd7d188c32277ba11182ced200b4726f280aa20
Billcountry <me@billcountry.tech>
2020-10-08 04:55:16 +0300

    IO writer to write to alt4

--- service/writer.go
+++ service/writer.go
@@ -1,1 +1,12 @@
 package service
+
+
+type alt4Writer struct {}
+
+func (writer alt4Writer) Write(p []byte) (n int, err error) {
+	message := string(p)
+	_, _err := Log(3, false, message, nil, 1).Result()
+	return len(p), _err
+}
+
+var Writer = alt4Writer{}
