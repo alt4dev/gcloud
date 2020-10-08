284bf3a38f40589714c52c8d85f2e06ff847960c
Billcountry <me@billcountry.tech>
2020-10-08 05:28:23 +0300

    Add a sync writer

--- service/writer.go
+++ service/writer.go
@@ -1,12 +1,20 @@
 package service
 
-
-type alt4Writer struct {}
+type alt4Writer struct{}
 
 func (writer alt4Writer) Write(p []byte) (n int, err error) {
+	message := string(p)
+	Log(3, false, message, nil, 1)
+	return len(p), nil
+}
+
+type alt4SyncWriter struct{}
+
+func (writer alt4SyncWriter) Write(p []byte) (n int, err error) {
 	message := string(p)
 	_, _err := Log(3, false, message, nil, 1).Result()
 	return len(p), _err
 }
 
 var Writer = alt4Writer{}
+var SyncWriter = alt4SyncWriter{}
