89f19f94890530eda16b7fab8b5d0906c272518a
Billcountry <me@billcountry.tech>
2020-10-10 21:33:11 +0300

    Writer goes a level further

--- service/writer.go
+++ service/writer.go
@@ -4,7 +4,7 @@ type alt4Writer struct{}
 
 func (writer alt4Writer) Write(p []byte) (n int, err error) {
 	message := string(p)
-	Log(3, false, message, nil, 1)
+	Log(4, false, message, nil, 1)
 	return len(p), nil
 }
 
@@ -12,7 +12,7 @@ type alt4SyncWriter struct{}
 
 func (writer alt4SyncWriter) Write(p []byte) (n int, err error) {
 	message := string(p)
-	_, _err := Log(3, false, message, nil, 1).Result()
+	_, _err := Log(4, false, message, nil, 1).Result()
 	return len(p), _err
 }
 
