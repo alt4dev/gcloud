7eed773261315fbc594312a3a23e18f48d7e8642
Billcountry <me@billcountry.tech>
2020-10-17 15:44:44 +0300

    Better Group Result documentation

--- log/groups.go
+++ log/groups.go
@@ -5,23 +5,19 @@ import (
 	"github.com/alt4dev/protobuff/proto"
 )
 
-// Group start a log group for the goroutine that calls this function. A group should be closed after. Use: `defer Group(title, claims).Close()`
-// Go routines are used to write logs to alt4 without blocking. You can use the module `sync/log` to write synchronously or call the function `Result` which is going to block if the operation is not done.
-func Group(title string, claims Claims) GroupResult {
-	return GroupResult{
-		logResult: service.Log(2,true, title, claims.parse(), 1),
-	}
-}
-
 // GroupResult Object returned by creating a new log group/thread.
 type GroupResult struct {
 	logResult *service.LogResult
 }
 
+// Return the result of the actual log event
 func (result GroupResult) Result() (*proto.Result, error) {
 	return result.logResult.Result()
 }
 
+// Close will mark the end of a thread closing the log group.
+// If there were unfinished writes to alt4 during this thread.
+// This method will wait for the writes to finish
 func (result GroupResult) Close() {
 	service.CloseGroup()
 }
