d2dda4f78814ef892e498422abe9a7acebaf8fd3
Billcountry <me@billcountry.tech>
2020-10-25 09:48:36 +0300

    Allow closing message when closing logs

--- log/groups.go
+++ log/groups.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"fmt"
 	"github.com/alt4dev/go/service"
 	"github.com/alt4dev/protobuff/proto"
 )
@@ -8,6 +9,7 @@ import (
 // GroupResult Object returned by creating a new log group/thread.
 type GroupResult struct {
 	logResult *service.LogResult
+	claims *Claims
 }
 
 // Return the result of the actual log event
@@ -16,8 +18,18 @@ func (result GroupResult) Result() (*proto.Result, error) {
 }
 
 // Close will mark the end of a thread closing the log group.
+// If arguments are provided to the close function, they'll be logged.
+// This can be useful for determining the latency of a request.
 // If there were unfinished writes to alt4 during this thread.
 // This method will wait for the writes to finish
-func (result GroupResult) Close() {
+func (result GroupResult) Close(v ...interface{}) {
+	if len(v) > 0{
+		var claims []*proto.Claim = nil
+		if result.claims != nil {
+			claims = result.claims.parse()
+		}
+		message := fmt.Sprint(v...)
+		service.Log(2, true, message, claims, proto.Log_NONE)
+	}
 	service.CloseGroup()
 }
