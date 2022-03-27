3166a14a8e9507d4f24f79589250fbf84d1fa7b4
Billcountry <me@billcountry.tech>
2022-03-27 19:14:14 +0300

    Simplify grouping and expect a request

--- log/groups.go
+++ log/groups.go
@@ -5,19 +5,35 @@ import (
 	"fmt"
 	"github.com/alt4dev/gcloud/service"
 	"net/http"
-	"net/url"
 	"runtime/debug"
 	"time"
 )
 
+// Labels are fields that will can be associated to your group log entry.
+// They can be used to filter and better identify your logs.
+type Labels map[string]interface{}
+
+func (labels Labels) parse() map[string]string {
+	protoLabels := map[string]string{}
+	for key, i := range labels {
+		var claimValue string
+		switch i.(type) {
+		case bool:
+			claimValue = fmt.Sprint(i.(bool))
+		case time.Time:
+			claimValue = i.(time.Time).Format("2006-01-02T15:04:05.000Z")
+		default:
+			claimValue = fmt.Sprint(i)
+		}
+		protoLabels[key] = claimValue
+	}
+	return protoLabels
+}
+
 // GroupResult Object returned by creating a new log group/thread.
 type GroupResult struct {
-	labels    *Labels
-	startTime time.Time
-	status    int
-	method    string
-	url       string
-	request   *http.Request
+	labels Labels
+	status int
 }
 
 // Close will mark the end of a thread closing the log group.
@@ -27,46 +43,21 @@ type GroupResult struct {
 // This method will wait for the writes to finish
 // Close also logs any panic but doesn't recover.
 func (result *GroupResult) Close() {
-	defer service.CloseGroup()
 	var labels map[string]string = nil
 	if result.labels != nil {
 		labels = result.labels.parse()
 	}
 
+	defer service.CloseGroup(result.status, labels)
+
 	r := recover()
-	// Recover any panic, just to log
+	// Recover any panic for recording, then continue
 	if r != nil {
-		service.Log(2, fmt.Sprint(r), nil, labels, logging.Critical, service.LogTime())
+		service.Log(2, fmt.Sprint(r), logging.Critical, service.LogTime())
 		// Log stack trace
-		service.Log(2, fmt.Sprint(string(debug.Stack())), nil, labels, logging.Critical, service.LogTime())
-	}
+		service.Log(2, fmt.Sprint(string(debug.Stack())), logging.Critical, service.LogTime())
 
-	// Parent log should have the last timestamp in the group
-	logTime := service.LogTime()
-
-	latency := logTime.Sub(result.startTime)
-
-	httpRequest := result.request
-	if httpRequest == nil {
-		_url, _ := url.Parse(result.url)
-		httpRequest = &http.Request{
-			Method: result.method,
-			URL:    _url,
-		}
-	}
-
-	// Complete the request
-	request := &logging.HTTPRequest{
-		Request: httpRequest,
-		Status:  result.status,
-		Latency: latency,
-	}
-
-	message := fmt.Sprintf("[%s] %s", result.method, result.url)
-
-	service.Log(2, message, request, labels, logging.Default, logTime)
-	// Anakin, continue panakin
-	if r != nil {
+		// Anakin, continue panakin
 		panic(r)
 	}
 }
@@ -76,7 +67,21 @@ func (result *GroupResult) SetStatus(httpStatus int) *GroupResult {
 	return result
 }
 
-func (result *GroupResult) SetRequest(request *http.Request) *GroupResult {
-	result.request = request
+func (result *GroupResult) SetLabel(key string, value interface{}) *GroupResult {
+	if result.labels == nil {
+		result.labels = Labels{}
+	}
+
+	result.labels[key] = value
 	return result
+}
+
+// Group start a log group for the goroutine that calls this function.
+// A group should be closed after. Use: `defer Group(...).Close()`
+func Group(request *http.Request, labels Labels) *GroupResult {
+	service.InitGroup(request)
+	return &GroupResult{
+		labels: labels,
+		status: 0,
+	}
 }
--- log/log.go
+++ log/log.go
@@ -19,92 +19,78 @@ var BuiltInExit func(code int) = func(code int) {
 	os.Exit(code)
 }
 
-// Group start a log group for the goroutine that calls this function.
-// A group should be closed after. Use: `defer Group(...).Close()`
-func Group(url string, method string) *GroupResult {
-	service.InitGroup()
-	return &GroupResult{
-		labels:    nil,
-		startTime: service.LogTime(),
-		status:    0,
-		method:    method,
-		url:       url,
-		request:   nil,
-	}
-}
-
 // Print send a log message to Google cloud logging
 func Print(v ...interface{}) {
 	message := fmt.Sprint(v...)
-	service.Log(2, message, nil, nil, logging.Default, service.LogTime())
+	service.Log(2, message, logging.Default, service.LogTime())
 }
 
 // Printf send a log message to Google cloud logging
 func Printf(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
-	service.Log(2, message, nil, nil, logging.Default, service.LogTime())
+	service.Log(2, message, logging.Default, service.LogTime())
 }
 
 // Println send a log message to Google cloud logging
 func Println(v ...interface{}) {
 	message := fmt.Sprintln(v...)
-	service.Log(2, message, nil, nil, logging.Default, service.LogTime())
+	service.Log(2, message, logging.Default, service.LogTime())
 }
 
 // Info send a logging.Info message to Google cloud logging
 func Info(v ...interface{}) {
 	message := fmt.Sprint(v...)
-	service.Log(2, message, nil, nil, logging.Info, service.LogTime())
+	service.Log(2, message, logging.Info, service.LogTime())
 }
 
 // Infof send a logging.Info message to Google cloud logging
 func Infof(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
-	service.Log(2, message, nil, nil, logging.Info, service.LogTime())
+	service.Log(2, message, logging.Info, service.LogTime())
 }
 
 // Debug send a logging.Debug message to Google cloud logging
 func Debug(v ...interface{}) {
 	message := fmt.Sprint(v...)
-	service.Log(2, message, nil, nil, logging.Debug, service.LogTime())
+	service.Log(2, message, logging.Debug, service.LogTime())
 }
 
 // Debugf send a logging.Debug message to Google cloud logging
 func Debugf(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
-	service.Log(2, message, nil, nil, logging.Debug, service.LogTime())
+	service.Log(2, message, logging.Debug, service.LogTime())
 }
 
 // Warning send a logging.Warning message to Google cloud logging
 func Warning(v ...interface{}) {
 	message := fmt.Sprint(v...)
-	service.Log(2, message, nil, nil, logging.Warning, service.LogTime())
+	service.Log(2, message, logging.Warning, service.LogTime())
 }
 
 // Warningf send a logging.Warning message to Google cloud logging
 func Warningf(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
-	service.Log(2, message, nil, nil, logging.Warning, service.LogTime())
+	service.Log(2, message, logging.Warning, service.LogTime())
 }
 
 // Error send a logging.Error message to Google cloud logging
 func Error(v ...interface{}) {
 	message := fmt.Sprint(v...)
-	service.Log(2, message, nil, nil, logging.Error, service.LogTime())
+	service.Log(2, message, logging.Error, service.LogTime())
 }
 
 // Errorf send a logging.Error message to Google cloud logging
 func Errorf(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
-	service.Log(2, message, nil, nil, logging.Error, service.LogTime())
+	service.Log(2, message, logging.Error, service.LogTime())
 }
 
 // Fatal This is equivalent to calling Print followed by os.Exit(1)
 // logged as logging.Alert
 func Fatal(v ...interface{}) {
 	message := fmt.Sprint(v...)
-	service.Log(2, message, nil, nil, logging.Alert, service.LogTime())
-	service.CloseGroup()
+	service.Log(2, message, logging.Alert, service.LogTime())
+	service.CloseGroup(0, nil)
 	BuiltInExit(1)
 }
 
@@ -112,8 +98,8 @@ func Fatal(v ...interface{}) {
 // logged as logging.Alert
 func Fatalf(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
-	service.Log(2, message, nil, nil, logging.Alert, service.LogTime())
-	service.CloseGroup()
+	service.Log(2, message, logging.Alert, service.LogTime())
+	service.CloseGroup(0, nil)
 	BuiltInExit(1)
 }
 
@@ -121,7 +107,7 @@ func Fatalf(format string, v ...interface{}) {
 // logged as logging.Critical
 func Panic(v ...interface{}) {
 	message := fmt.Sprint(v...)
-	service.Log(2, message, nil, nil, logging.Critical, service.LogTime())
+	service.Log(2, message, logging.Critical, service.LogTime())
 	BuiltInPanic(message)
 }
 
@@ -129,6 +115,6 @@ func Panic(v ...interface{}) {
 // logged as logging.Critical
 func Panicf(format string, v ...interface{}) {
 	message := fmt.Sprintf(format, v...)
-	service.Log(2, message, nil, nil, logging.Critical, service.LogTime())
+	service.Log(2, message, logging.Critical, service.LogTime())
 	BuiltInPanic(message)
 }
